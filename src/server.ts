ff6b0475da1564cc98404c4765c1ccff1d734bb3
vladji <vladjislav@gmail.com>
2025-06-18 22:59:02 +0700

    feat: CORS

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
+import express from "express";
+import compression from "compression";
+import cors from "cors";
 
+const app = express();
+
+app.use(compression());
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://kit-food.net"],
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
